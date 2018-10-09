61235f159a47e81316b5509da084245af4728419
Andy Wermke <andy.dev@next-step-software.com>
2018-10-09 09:20:12 +0200

    Improve `ts search` result ordering

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -25,10 +25,36 @@ async function fetchTypePackageNames (partialPackageName: string) {
   return results
 }
 
-function sortResults (results: Result[]): Result[] {
+function filterResults (results: Result[]): Result[] {
+  return results.filter(
+    result => result.name[0].startsWith("@types/") || result.name[0].match(/^@[^\/]+\/types-/)
+  )
+}
+
+function getTypePackageBaseName (packageName: string) {
+  return packageName.replace(/^@types\//, "").replace(/^@[^\/]+\/types-/, "")
+}
+
+function sortResults (results: Result[], packageName: string): Result[] {
+  const customTypesPackageNames = results.filter(result => !result.name[0].startsWith("@types/"))
+  const defTypedPackageNames = results.filter(result => result.name[0].startsWith("@types/"))
+
+  const sorter = (result1: Result, result2: Result) => {
+    const baseName1 = getTypePackageBaseName(result1.name[0])
+    const baseName2 = getTypePackageBaseName(result2.name[0])
+
+    if (baseName2.startsWith(packageName) && !baseName1.startsWith(packageName)) {
+      return 1
+    } else if (baseName2.length < baseName1.length) {
+      return 1
+    } else {
+      return -1
+    }
+  }
+
   return [
-    ...results.filter(result => result.name[0].startsWith("@types/")),
-    ...results.filter(result => !result.name[0].startsWith("@types/"))
+    ...defTypedPackageNames.sort(sorter),
+    ...customTypesPackageNames.sort(sorter)
   ]
 }
 
@@ -44,9 +70,10 @@ export async function run (cli: CLI) {
   if (results.length === 0) {
     console.log(`${figures.cross} No matches`)
   } else {
+    const preparedResults = sortResults(filterResults(results), packageName)
     console.log("Type declaration packages:\n")
 
-    for (const result of sortResults(results)) {
+    for (const result of preparedResults) {
       console.log(`  ${result.name[0]}`)
     }
   }
